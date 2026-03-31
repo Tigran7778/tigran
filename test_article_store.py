49dcfe0cc1547e5a75ca530d91691b9280783abf
Tigran Gevorkyan <tigr553553@gmail.com>
2026-03-31 11:24:05 +0300

    Добавил начальный код

---
+++ test_article_store.py
@@ -0,0 +1,10 @@
+from playwright.sync_api import Page, expect
+
+def test_login_page_title(page: Page):
+    page.goto('http://144.31.139.115:5000/')
+    page.locator('xpath=//a[contains(@class, "login")]').click()
+    expect(page).to_have_title('Login')
+    page.locator('//*[@id="username"]').fill('ghhgfhj')
+    page.locator('//*[@id="password"]').fill('rt37429')
+    page.keyboard.press('Enter')
+    expect(page.locator("//div[contains(@class, 'login-error')]")).to_have_text("Invalid login or password.")
